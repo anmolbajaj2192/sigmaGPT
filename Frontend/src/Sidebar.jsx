b63645189a987103a71297add44baf0e00b68a18
Anmol Bajaj <banmol241@gmail.com>
2025-11-25 00:00:01 +0530

    styling sidebar

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -6,19 +6,19 @@ function Sidebar() {
             {/* new chat button */}
             <button>
                     <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo"/>
-                    <i className="fa-solid fa-pen-to-square"></i>
+                    <span><i className="fa-solid fa-pen-to-square"></i></span>
             </button>
 
             {/* history */}
             <ul className="history">
-                <li>history1</li>
-                <li>history2</li>
-                <li>history3</li>
+                <li>thread1</li>
+                <li>thread2</li>
+                <li>thread3</li>
             </ul>
 
             {/* sign */} 
             <div className="sign">
-                <p>By Apna College &hearts;</p>
+                <p>By Anmol Bajaj &hearts;</p>
             </div>
 
         </section>
